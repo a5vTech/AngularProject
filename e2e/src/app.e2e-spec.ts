0b216f9b32d75a637727eb63335c08420f8c1d36
a5vTech <a5vgaminghq@gmail.com>
2019-03-27 20:55:51 +0100

    Finished all e2e tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -28,6 +28,42 @@ describe('workspace-project App', () => {
   });
 
 
+  it('3.0 Test that a new quiz is created', () => {
+    browser.get('portal/display-quizzes');
+    // browser.get('portal/create-quiz');
+    element.all(by.css('.cards')).then((elemsAfter) => {
+      let start = elemsAfter.length;
+      element(by.id('create-quiz-btn')).click();
+      element(by.id('quiztitle')).sendKeys('super fed test quiz');
+      element(by.id('new-question')).click();
+      element(by.id('question-title')).sendKeys('This is the quiz title');
+      element.all(by.css(".option")).get(0).sendKeys('Quiz option 1');
+      element.all(by.css(".option")).get(1).sendKeys('Quiz option 2');
+      element(by.id('save-quiz')).click();
+      element.all(by.css('.cards')).then((elementsAfter) => {
+        let endVal = elementsAfter.length;
+        expect(endVal).toEqual(+start + 1);
+
+      });
+    });
+
+  });
+
+  it('4.0 Test that deletes a quiz', () => {
+    element.all(by.css('.cards')).then((elemsAfter) => {
+
+      let start = elemsAfter.length;
+      element.all(by.css(".delete-btn")).get(start-1).click();
+      element.all(by.css('.cards')).then((elementsAfter) => {
+        let endVal = elementsAfter.length;
+        expect(endVal).toEqual(+start - 1);
+
+      });
+    });
+
+  });
+
+
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
